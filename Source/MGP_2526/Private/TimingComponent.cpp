eacde585132daf26556153f7051f7304e4732273
mariogithub <bagyinszki.mario1@gmail.com>
2026-03-30 13:46:09 +0100

    created all classes that are needed

---
+++ Source/MGP_2526/Private/TimingComponent.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimingComponent.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UTimingComponent::UTimingComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+	bIsActive = false;
+
+	
+	Window.PerfectStart = 0.2f;
+	Window.PerfectEnd = 0.3f;
+
+	Window.GoodStart = 0.1f;
+	Window.GoodEnd = 0.4f;
+
+	Window.OkStart = 0.0f;
+	Window.OkEnd = 0.5f;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTimingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+void UTimingComponent::StartTimingWindow(ETimingAction ActionType)
+{
+	CurrentAction = ActionType;
+	StartTime = GetWorld()->GetTimeSeconds();
+	bIsActive = true;
+}
+
+ETimingResult UTimingComponent::EvaluateTiming()
+{
+    if (!bIsActive)
+        return ETimingResult::Miss;
+
+    float CurrentTime = GetWorld()->GetTimeSeconds() - StartTime;
+
+    if (CurrentTime >= Window.PerfectStart && CurrentTime <= Window.PerfectEnd)
+    {
+        bIsActive = false;
+        return ETimingResult::Perfect;
+    }
+
+    if (CurrentTime >= Window.GoodStart && CurrentTime <= Window.GoodEnd)
+    {
+        bIsActive = false;
+        return ETimingResult::Good;
+    }
+
+    if (CurrentTime >= Window.OkStart && CurrentTime <= Window.OkEnd)
+    {
+        bIsActive = false;
+        return ETimingResult::Ok;
+    }
+
+    bIsActive = false;
+    return ETimingResult::Miss;
+}
+
---
+++ Source/MGP_2526/Public/TimingComponent.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TimingComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class ETimingAction : uint8
+{
+	None,
+	Parry,
+	Block,
+	SpecialAttack
+};
+
+UENUM(BlueprintType)
+enum class ETimingResult : uint8
+{
+	None,
+	Ok,
+	Good,
+	Perfect,
+	Miss
+};
+USTRUCT(BlueprintType)
+struct FTimingWindow
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PerfectStart;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PerfectEnd;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GoodStart;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GoodEnd;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float OkStart;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float OkEnd;
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MGP_2526_API UTimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTimingComponent();
+	void StartTimingWindow(ETimingAction ActionType);
+	ETimingResult EvaluateTiming();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	float StartTime;
+	bool bIsActive;
+
+	ETimingAction CurrentAction;
+
+	UPROPERTY(EditAnywhere)
+	FTimingWindow Window;
+		
+};
